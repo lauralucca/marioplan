3a2a0e6357927d5ad3d0ca4fdfbd15d59a27d359
Laura Lourenco Lucca <laura.lourenco.lucca@everis.com>
2019-04-02 16:28:39 -0300

    substituindo conteúdo mockado na pagina de detalhes do projeto

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">
-                        Project Title - { id }
-                    </span>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora accusamus illo velit officiis expedita porro animi maxime fuga veniam voluptates eaque amet nobis alias illum, eum magnam adipisci a quis?</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Laura</div>
-                    <div>2nd September, 2am</div>
+    const { project } = props;
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">
+                        { project.title }
+                        </span>
+                        <p>{ project.content }</p>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
+                        <div>2nd September, 2am</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+    
+        )
+    }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id
+    const projects = state.firestore.data.projects
+    const project = projects ? projects[id] : null
+    return {
+        project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails)
